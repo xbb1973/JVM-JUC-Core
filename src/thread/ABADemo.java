a16402a69a7d5239dd4c3907bafda3407a47d0bf
xbb1973 <xiaobanban1973@163.com>
2020-04-11 16:03:33 +0800

    update code & xmind of CAS & ABA module

--- src/thread/ABADemo.java
+++ src/thread/ABADemo.java
@@ -14,6 +14,7 @@ public class ABADemo {
         new Thread(() -> {
             atomicReference.compareAndSet(100, 101);
             atomicReference.compareAndSet(101, 100);
+            System.out.println("t1:'狸猫换太子100->101->100，别担心，t2不知道它老婆被我换了'");
         }, "t1").start();
 
         new Thread(() -> {
@@ -23,6 +24,7 @@ public class ABADemo {
                 e.printStackTrace();
 
             }
+            System.out.println("t2:'我老婆还是我老婆，嘿嘿嘿，100->2019'");
             System.out.println(atomicReference.compareAndSet(100, 2019) + "\t" + atomicReference.get().toString());
         }, "t2").start();
 
@@ -39,14 +41,17 @@ public class ABADemo {
             atomicStampedReference.compareAndSet(101,100,
                     atomicStampedReference.getStamp(),atomicStampedReference.getStamp()+1);
             System.out.println(Thread.currentThread().getName() + "\t第三次版本号： " + atomicStampedReference.getStamp());
+            System.out.println("t3:'狸猫换太子100->101->100，别担心，t4不知道它老婆被我换了'");
         }, "t3").start();
 
         new Thread(() -> {
             int stamp = atomicStampedReference.getStamp();
             System.out.println(Thread.currentThread().getName() + "\t第一次版本号： " + stamp);
+            System.out.printf("t4:'这是我刚买的老婆，版本号为%d'\n", stamp);
             try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
             boolean result=atomicStampedReference.compareAndSet(100,2019,
                     stamp,stamp+1);
+            System.out.println("t4:'谁他妈把我老婆换了！！！！'");
             System.out.println(Thread.currentThread().getName()+"\t修改成功与否："+result+"  当前最新版本号"+atomicStampedReference.getStamp());
             System.out.println(Thread.currentThread().getName()+"\t当前实际值："+atomicStampedReference.getReference());
         }, "t4").start();
--- src/thread/VolatileDemo.java
+++ src/thread/VolatileDemo.java
@@ -11,61 +11,64 @@ public class VolatileDemo {
 
     private static void atomicDemo() {
         System.out.println("原子性测试");
-        MyData myData=new MyData();
+        MyData myData = new MyData();
         for (int i = 1; i <= 20; i++) {
-            new Thread(()->{
-                for (int j = 0; j <1000 ; j++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
                     myData.addPlusPlus();
                     myData.addAtomic();
                 }
-            },String.valueOf(i)).start();
+            }, String.valueOf(i)).start();
         }
-        while (Thread.activeCount()>2){
+        // 默认有gc线程和main线程，main线程等待上述线程执行结束
+        while (Thread.activeCount() > 2) {
             Thread.yield();
         }
-        System.out.println(Thread.currentThread().getName()+"\t int type finally number value: "+myData.number);
-        System.out.println(Thread.currentThread().getName()+"\t AtomicInteger type finally number value: "+myData.atomicInteger);
+        System.out.println(Thread.currentThread().getName() + "\t int type finally number value: " + myData.number);
+        System.out.println(Thread.currentThread().getName() + "\t AtomicInteger type finally number value: " + myData.atomicInteger);
     }
 
     //volatile可以保证可见性，及时通知其它线程主物理内存的值已被修改
     private static void volatileVisibilityDemo() {
         System.out.println("可见性测试");
-        MyData myData=new MyData();//资源类
+        MyData myData = new MyData();//资源类
         //启动一个线程操作共享数据
-        new Thread(()->{
-            System.out.println(Thread.currentThread().getName()+"\t come in");
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "\t come in");
             try {
                 TimeUnit.SECONDS.sleep(3);
                 myData.setTo60();
-                System.out.println(Thread.currentThread().getName()+"\t update number value: "+myData.number);
-            }catch (InterruptedException e)
-            {
+                System.out.println(Thread.currentThread().getName() + "\t update number value: " + myData.number);
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        },"AAA").start();
+        }, "AAA").start();
 
-        while (myData.number==0){
+        while (myData.number == 0) {
             //main线程持有共享数据的拷贝，一直为0
         }
-        System.out.println(Thread.currentThread().getName()+"\t mission is over. main get number value: "+myData.number);
+        System.out.println(Thread.currentThread().getName() + "\t mission is over. main get number value: " + myData.number);
     }
 }
 
-class MyData{
-    int number=0;
-    //volatile int number=0;
+class MyData {
+    // int number=0;
+    volatile int number = 0;
 
-    AtomicInteger atomicInteger=new AtomicInteger();
-    public void setTo60(){
-        this.number=60;
+    AtomicInteger atomicInteger = new AtomicInteger();
+
+    public void setTo60() {
+        this.number = 60;
     }
 
     //此时number前面已经加了volatile，但是不保证原子性
-    public void addPlusPlus(){
+    // public synchronized void addPlusPlus() {
+    public void addPlusPlus() {
         number++;
     }
 
-    public void addAtomic(){
-        atomicInteger.getAndIncrement();
+    public void addAtomic() {
+        // atomicInteger.getAndIncrement();
+        atomicInteger.addAndGet(2);
     }
 }
