b1f0eee873979a2d62d123b74431dd4ebe4dffc0
xbb1973 <xiaobanban1973@163.com>
2020-05-03 00:16:02 +0800

    20/05/02,update Basic JVM xmind&code

--- src/jvm/GCOverheadDemo.java
+++ src/jvm/GCOverheadDemo.java
@@ -5,7 +5,25 @@ import java.util.List;
 
 //-Xms10m -Xmx10m -XX:MaxDirectMemorySize=5m -XX:+PrintGCDetails
 public class GCOverheadDemo {
+    private byte[] bigSize = new byte[2*1024*1024];
+    Object instance = null;
+
     public static void main(String[] args) {
+        // autoGC();
+        referenceCountGC();
+    }
+
+    private static void referenceCountGC() {
+        GCOverheadDemo overheadDemo1 = new GCOverheadDemo();
+        GCOverheadDemo overheadDemo2 = new GCOverheadDemo();
+        overheadDemo1.instance = overheadDemo2;
+        overheadDemo2.instance = overheadDemo1;
+        overheadDemo1 = null;
+        overheadDemo2 = null;
+        System.gc();
+    }
+
+    private static void autoGC() {
         int i = 0;
         List<String> list = new ArrayList<>();
         try {
--- src/thread/SingletonDemo.java
+++ src/thread/SingletonDemo.java
@@ -2,14 +2,14 @@ package thread;
 
 public class SingletonDemo {
     // private static SingletonDemo singletonDemo = null;
-    private static volatile SingletonDemo singletonDemo=null;
+    private static volatile SingletonDemo singletonDemo = null;
+
     private SingletonDemo() {
         System.out.println(Thread.currentThread().getName() + "\t 我是构造方法");
     }
 
     //DCL模式 Double Check Lock 双端检索机制：在加锁前后都进行判断
     public static /*synchronized*/ SingletonDemo getInstance() {
-
 
         if (singletonDemo == null) {
             synchronized (SingletonDemo.class) {
